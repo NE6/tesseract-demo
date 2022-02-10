afed852c679af2c331e2564181b0a7a993c2d43b
Paul Craven <paul.craven@ne6.studio>
2022-02-10 16:23:38 +0000

    constrained height of cam field. added regex to exclude image noise

--- pages/third.tsx
+++ pages/third.tsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { createWorker } from 'tesseract.js';
+import { createWorker, createScheduler } from 'tesseract.js';
 import Webcam from 'react-webcam';
 import { Box, Flex, Button, Image, Text } from '@chakra-ui/react';
+import { m } from 'framer-motion';
 
 export default function Third() {
   const webcamRef = React.useRef(null);
   const [URI, setURI] = useState(null);
   const [ocr, setOcr] = useState('Start Scanning');
-
-  const worker = createWorker();
-  // {
-  // logger: (m) => console.log(m),
-  // }
+  const worker = createWorker({
+    logger: (m) => console.log(m),
+  });
   const doOCR = async (URI) => {
     await worker.load();
     await worker.loadLanguage('eng');
@@ -20,9 +19,18 @@ export default function Third() {
       const {
         data: { text },
       } = await worker.recognize(URI);
-      console.log(text);
-      setOcr(text);
-      worker.terminate();
+
+      const matchedText = text.match(
+        /[a-zA-Z]+-[a-zA-Z]+\d\d[a-zA-Z]\/[a-zA-Z]\d[a-zA-Z]\d-[a-zA-Z]/
+      );
+      if (matchedText) {
+        console.log(matchedText[0]);
+        setOcr(matchedText[0]);
+      } else {
+        setOcr('Please try again');
+        return;
+      }
+      await worker.terminate();
       return { text };
     }
   };
@@ -32,6 +40,7 @@ export default function Third() {
 
   const capture = React.useCallback(() => {
     // setURI(null);
+    setOcr('Processing...');
     const imageSrc = webcamRef.current.getScreenshot();
     setURI(imageSrc);
     doOCR(imageSrc);
@@ -45,6 +54,8 @@ export default function Third() {
         screenshotFormat="image/png"
         videoConstraints={{
           facingMode: 'user',
+          height: 100,
+          width: 500,
           // { exact: 'environment' }
         }}
       />
@@ -52,7 +63,7 @@ export default function Third() {
         <Button onClick={capture}>Capture Image</Button>
         <Text fontSize="30px">{ocr}</Text>
       </Flex>
-      {console.log(ocr)}
+      {/* {console.log(ocr)} */}
     </Box>
   );
 }
