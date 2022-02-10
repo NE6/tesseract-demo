02c25999552608a58ed6ccc8e06f5897f17161f7
Paul Craven <paul.craven@ne6.studio>
2022-02-10 12:59:11 +0000

    set to environment camera for phone

--- pages/third.tsx
+++ pages/third.tsx
@@ -64,7 +64,7 @@ export default function Third() {
         ref={webcamRef}
         screenshotFormat="image/png"
         videoConstraints={{
-          facingMode: 'FACING_MODE_ENVIRONMENT',
+          facingMode: { exact: 'environment' },
         }}
       />
       <Flex>
