1a6fda62501c20aeca4a3430a9719c93d3a009b5
Paul Craven <paul.craven@ne6.studio>
2022-02-10 11:58:14 +0000

    basic working version

--- pages/third.tsx
+++ pages/third.tsx
@@ -1,35 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import { createWorker } from 'tesseract.js';
 import Webcam from 'react-webcam';
-import { Box, Button, Image } from '@chakra-ui/react';
+import { Box, Flex, Button, Image, Text } from '@chakra-ui/react';
 
 export default function Third() {
   const webcamRef = React.useRef(null);
-  const [image, setImage] = useState('');
+  const [URI, setURI] = useState('');
+  const [ocr, setOcr] = useState('');
+  let context, image;
 
-  // initialize Tesseract worker
-  useEffect(() => {
-    start();
-  }, []);
-  const start = async () => {
-    const worker = createWorker({
-      logger: (m) => console.log(m),
-    });
+  const worker = createWorker({
+    logger: (m) => console.log(m),
+  });
+  const doOCR = async (URI) => {
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
+
+    const {
+      data: { text },
+    } = await worker.recognize(
+      // 'https://tesseract.projectnaptha.com/img/eng_bw.png'
+      // PNGImage
+      URI
+    );
+    console.log(URI);
+    setOcr(text);
+    console.log(text);
+    await worker.terminate();
   };
+  // useEffect(() => {
+  //   doOCR();
+  // });
 
   const capture = React.useCallback(() => {
+    setURI('');
     const imageSrc = webcamRef.current.getScreenshot();
-    setImage(imageSrc);
-  }, [webcamRef]);
+    setURI(imageSrc);
+    // console.log(image);
+    doOCR(URI);
+  }, [webcamRef, setURI, URI]);
 
   return (
     <Box>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
-      <Button onClick={capture}>Capture Image</Button>
-      {image ? <Image src={image} alt="image"></Image> : null}
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/png" />
+      <Flex>
+        <Button onClick={capture}>Capture Image</Button>
+        <Text fontSize="30px">{ocr}</Text>
+      </Flex>
+      {/* <Box>{PNGImage}</Box> */}
+      {console.log(ocr)}
     </Box>
   );
 }
